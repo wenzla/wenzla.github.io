5d67f18adb790878fec1c5b6d9a5dc0093beac65
wenzla <allenliwenzl@gmail.com>
2017-06-27 09:00:32 -0400

    Added angular scripts

--- script/test.js
+++ script/test.js
@@ -1,8 +1,1 @@
-angular.module('lineApp', [])
-  .controller('LineListController', function() {
-    var lineList = this;
-    lineList.lines = [
-      {text:'1', show: true},
-	  {text:'2', show: false},
-      {text:'3', show: false}];
-  });
+// test js
