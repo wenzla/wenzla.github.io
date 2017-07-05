c3c874be7d64fe4e1821def63f7181a0261d0a41
wenzla <allenliwenzl@gmail.com>
2017-07-05 11:53:12 -0400

    Added descriptions to popovers

--- script/index.js
+++ script/index.js
@@ -2,7 +2,7 @@ angular.module('link', [])
   .controller('linkController', function() {
     var linkList = this;
     linkList.links = [
-      {url:'flowCalc', id: 'chuCalc', title:'Parker Flow Rate Calculator', text: 'Parker Flow Calculator'},
+      {url:'flowCalc',  id: 'chuCalc', title:'Parker Flow Rate Calculator', text: 'Parker Flow Calculator'},
 	  {url:'video', id: 'videoPage', title:'Parker Long Video', text: 'Parker Long Video'},
       {url:'testApp', id: 'testPage', title:'Testing Locations', text: 'Test Area'}];
   });
@@ -11,19 +11,22 @@ $(document).ready(function(){
 	$('#testPage').popover({
 		placement : 'bottom',
 		trigger : 'hover',
+		delay: {show: 200, hide: 200},
 		html : true,
-		content : '<img src="images/testPage.png" class="media-object">'
+		content : '<p>Test Area to play around with new features.</p><img src="images/testPage.png" class="media-object center-block">'
 	});
 	$('#videoPage').popover({
 		placement : 'bottom',
 		trigger : 'hover',
+		delay: {show: 200, hide: 200},
 		html : true,
-		content : '<img src="images/intro.PNG" class="media-object thumbnail">'
+		content : '<p>A simple chapter selector for a video made for Parker.</p><img src="images/intro.PNG" class="center-block media-object thumbnail">'
 	});
 	$('#chuCalc').popover({
 		placement : 'bottom',
 		trigger : 'hover',
+		delay: {show: 200, hide: 200},
 		html : true,
-		content : '<img src="images/calcTooltip.png" class="media-object">'
+		content : '<p>Flow calculator to determine the flow rate and V/L ratio of Parker FTS series pumps given an environment setup.</p> <img src="images/calcTooltip.png" class="media-object center-block">'
 	});
 });
