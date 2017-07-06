e40dcb566207ff4fcfb25fdcd2280514834621be
wenzla <allenliwenzl@gmail.com>
2017-07-06 11:25:27 -0400

    New test

--- script/test.js
+++ script/test.js
@@ -1,1 +1,6 @@
 // test js
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+function addHeading(){
+	
+}
+//$("#buildArea").append("<div class=\"col-sm-1\"> <p>&nbsp;</p> <button type=\"button\" class=\"btn btn-xs btn-default customBtn\"> <span class=\"glyphicon glyphicon-remove\"></span></button> </div>");	
