a7dcccd96ecbaa42cc959243f5d535bcb40c102d
wenzla <allenliwenzl@gmail.com>
2017-07-11 14:18:00 -0400

    Added more features
    Added ability to change color; TODO: add text color changer
    
    Also added checkboxes for logos and buttons in the future.

--- script/email.js
+++ script/email.js
@@ -6,16 +6,18 @@ var last = new Array();
 var lastIndex = 0;
 var outputString = new Array();
 outputString.push("\n");
-outputString.push("<!DOCTYPE html><html><body style=\"color: black; background-color: white\">");
+outputString.push("<!DOCTYPE html><html><body>");
 function makeHeading(){
 	var title = $("#titleGen").val();
 	var date = $("#dateGen").val();
-	var isChecked = $('input').is(':checked');
+	var color = $("#TitleColor").val();
+	var headColor = $("#HeadingColor").val();
+	var isChecked = $('.dateCheckbox').is(':checked');
 	if (isChecked){
 		var today = new Date();
 		date = (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear();
 	}
-	var stringGen = "<div id=\"heading" + headingCounter + "\" style=\"background-color: #ddd\"><div style=\"text-align: center; font-weight: bold; font-size: 3ex; padding-top: 1.5ex; color: rgb(78,216,255)\">" + title + "</div><div style=\"text-align: center; padding-top: 1.5ex; padding-bottom: 1.5ex; color: black; \"> Announcements for " + date + "</div></div>";
+	var stringGen = "<div id=\"heading" + headingCounter + "\" style=\"background-color: " + headColor + "\" class=\"HeadingClass\"><div class=\"HeadingTitleClass\" style=\"text-align: center; font-weight: bold; font-size: 3ex; padding-top: 1.5ex; color:" + color + "\">" + title + "</div><div class=\"TextClass\" style=\"text-align: center; padding-top: 1.5ex; padding-bottom: 1.5ex; color: black; \"> Announcements for " + date + "</div></div>";
 	$("#buildArea").append(stringGen);
 	outputString.push(stringGen);
 	headingCounter += 1;
@@ -28,7 +30,9 @@ function makeHeading(){
 
 function makeSection(){
 	var title = $("#StitleGen").val();
-	var stringGen = "<div id=\"section" + sectionCounter + "\" style=\"font-size: 3.5ex; font-weight: bold; text-align: center; padding-bottom: 1ex; padding-top: 1ex; color: rgb(78,216,255)\">" + title + "<hr></div>";
+	var color = $("#TitleColor").val();
+	var backColor = $("#BackGroundColor").val();
+	var stringGen = "<div id=\"section" + sectionCounter + "\" style=\"font-size: 3.5ex; font-weight: bold; text-align: center; padding-bottom: 1ex; padding-top: 1ex; background-color: " + backColor + "; color: " + color + "\" class=\"SectionClass\">" + title + "<hr></div>";
 	$("#buildArea").append(stringGen);
 	outputString.push(stringGen);
 	sectionCounter += 1;
@@ -41,7 +45,8 @@ function makeSection(){
 function makeSSection(){
 	var SSname = $("#SSnameGen").val();
 	var SStext = $("#SStextGen").val();
-	var stringGen = "<div id=\"Ssection" + SsectionCounter + "\"><div style=\"padding-left: 5%; padding-right: 5%; font-size: 2ex; font-weight: bold; padding-bottom: 0.5ex;\">" + SSname + "</div><p style=\"padding-left: 5%; padding-right: 5%\">" + SStext + "</p></div>";
+	var backColor = $("#BackGroundColor").val();
+	var stringGen = "<div id=\"Ssection" + SsectionCounter + "\"><div class=\"SSectionClass\" style=\"padding-left: 5%; padding-right: 5%; font-size: 2ex; background-color: " + backColor + "; font-weight: bold; padding-bottom: 0.5ex;\">" + SSname + "</div><p class=\"SSectionClass\" style=\"padding-left: 5%; padding-right: 5%; background-color: " + backColor + ";\">" + SStext + "</p></div>";
 	$("#buildArea").append(stringGen);
 	outputString.push(stringGen);
 	SsectionCounter += 1;
@@ -89,6 +94,9 @@ function output(){
 		$("#popupText").append(outputString[i]);
 	}
 	outputString.pop();
+	$('#copyPopup').modal({
+		backdrop: 'static'
+	});
 }
 
 function dismissPopup(){
@@ -114,7 +122,15 @@ function copy(){
 
 }
 
-
+function addAtag(){
+	var Htext = $("#Htext").val();
+	var Hlink = $("#Hlink").val();
+	var correctURLFormat = new RegExp("\b(http)s?(:\/\/).*\..*\b");
+	if(!correctURLFormat.test(Hlink)){
+		Hlink = "http://" + Hlink;
+	}
+	$( "#SStextGen" ).append( '&lt;' + "a href=\"" + Hlink + "\">" + Htext + '&lt;' + "/a>");
+}
 
 
 
